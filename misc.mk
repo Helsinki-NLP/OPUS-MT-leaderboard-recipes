8bf64a4db9c8d5b7cb209185cccc99b92a7ffe02
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2023-06-10 14:56:54 +0300

    add remove recipe

--- misc.mk
+++ misc.mk
@@ -30,3 +30,45 @@ benchmark-info:
 
 print-makefile-variables:
 	$(foreach var,$(.VARIABLES),$(info $(var) = $($(var))))
+
+
+
+
+#--------------------
+# `make create-model-zipfiles`
+#
+# make a new zip file that only includes logfiles from evaluation
+# translation files will stay in model sub dirs
+# also create system output files from the "compare" files if necessary
+#--------------------
+
+EVALZIP_FILES    := $(shell find ${REPOHOME}models -name '*.eval.zip')
+MODELZIP_FILES   := $(patsubst %.eval.zip,%.zip,${EVALZIP_FILES})
+
+ifneq (${MODEL},)
+ifneq ($(wildcard ../models/${MODEL}),)
+  COMPARE_FILES    := $(wildcard ../models/${MODEL}/*.compare)
+  OUTPUT_FILES     := $(patsubst %.compare,%.output,${COMPARE_FILES})
+endif
+endif
+
+create-model-zipfiles: ${MODELZIP_FILES}
+
+${MODELZIP_FILES}: %.zip: %.eval.zip
+	mkdir -p $(@:.zip=)
+	cd $(@:.zip=) && unzip -u ../$(notdir $<)
+	cd $(@:.zip=) && find . -type f -not -name '*.compare' -not -name '*.output' | xargs zip ../$(notdir $@)
+	find $(@:.zip=) -type f -not -name '*.compare' -not -name '*.output' -delete
+	if [ `find $(@:.zip=) -name '*.compare' | wc -l` -gt `find $(@:.zip=) -name '*.output | wc -l` ]; then \
+	  find $(@:.zip=) -name '*.compare' -exec \
+	  sh -c 'i={}; o=$(echo $$i | sed "s/.compare/.output/"); if [ ! -e $$o ]; then sed -n "3~4p" $$i > $$o; fi' \; \
+	fi
+#	${MAKE} MODEL=$(patsubst ../models/%,%,$(@:.zip=)) create-output-files
+
+create-output-files: ${OUTPUT_FILES}
+
+%.output: %.compare
+	sed -n '3~4p' $< > $@
+
+
+
