df1fda4bb9322b975207fafb5e83502b7050c8e6
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2023-06-17 23:57:27 +0200

    remove eval files from zip

--- misc.mk
+++ misc.mk
@@ -72,9 +72,20 @@ create-output-files: ${OUTPUT_FILES}
 	sed -n '3~4p' $< > $@
 
 
-EVAL_IN_ZIP_FILES   := $(patsubst %.zip,%.eval-files,${MODELZIP_FILES})
 
-extract-eval-files: ${EVAL_IN_ZIP_FILES}
+## some more house-keeping
+## - extract eval files from zip files
+## - delete eval files from zip files
 
-${EVAL_IN_ZIP_FILES}: %.eval-files: %.zip
-	cd $(@:.eval-files=) && unzip -u ../$(notdir $<) '*.eval'
+EXTRACT_EVAL_IN_ZIP_FILES := $(patsubst %.zip,%.extact-eval-files,${MODELZIP_FILES})
+DELETE_EVAL_IN_ZIP_FILES := $(patsubst %.zip,%.delete-eval-files,${MODELZIP_FILES})
+
+extract-eval-files: ${EXTRACT_EVAL_IN_ZIP_FILES}
+
+${EXTRACT_EVAL_IN_ZIP_FILES}: %.extract-eval-files: %.zip
+	-cd $(@:.extract-eval-files=) && unzip -u ../$(notdir $<) '*.eval'
+
+delete-eval-files: ${DELETE_EVAL_IN_ZIP_FILES}
+
+${DELETE_EVAL_IN_ZIP_FILES}: %.delete-eval-files: %.zip
+	-cd $(@:.delete-eval-files=) && zip -d ../$(notdir $<) '*.eval'
