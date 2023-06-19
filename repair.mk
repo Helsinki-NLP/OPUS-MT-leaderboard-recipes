dcc2403b71124ecbd9dd53d5cf26d49856bd484e
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2023-06-20 00:04:22 +0200

    repair repo files

--- repair.mk
+++ repair.mk
@@ -1,38 +1,8 @@
 # -*-makefile-*-
-
-
-
-SCOREFILES_VALIDATED = $(patsubst %,%.validated,${SCOREFILES})
-
-validate-all-scorefiles:
-	for s in ${ALL_SOURCES}; do \
-	  ${MAKE} SOURCE=$$s ALL_MODELS=1 validate-scorefiles; \
-	done
-
-validate-scorefiles: ${SCOREFILES_VALIDATED}
-
-${SCOREFILES_VALIDATED}: %.validated: %
-	@if [ `sort -u $< | wc -l` != `cat $< | wc -l` ]; then \
-	  echo "$< has duplicated lines"; \
-	  mv -f $< $@; \
-	  sort -u $@ > $<; \
-	fi
-	@touch $@
-
-
-benchmark-info:
-	@echo "benchmarks: ${TESTSETS}"
-	@echo "  selected: ${TESTSET}"
-	@echo "    source: ${TESTSET_SRC}"
-	@echo "trg-labels: ${TESTSET_LABELS}"
-	@echo "references: ${TESTSET_REFS}"
-
-
-print-makefile-variables:
-	$(foreach var,$(.VARIABLES),$(info $(var) = $($(var))))
-
-
-
+#
+# various targets to repair certain files
+# (changes of repo structure etc)
+#
 
 #--------------------
 # `make create-model-zipfiles`
